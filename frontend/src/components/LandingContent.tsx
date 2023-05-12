c2a90b161bf4feea771088ec33afa9a003cb4774
d4ndel1on <stefan@fancynetwork.net>
2023-05-12 10:32:22 +0200

    add qr-code

--- frontend/src/components/LandingContent.tsx
+++ frontend/src/components/LandingContent.tsx
@@ -1,14 +1,25 @@
-import {Container} from "@mui/material";
+import {Container, Grid} from "@mui/material";
 import React from "react";
 import {NewsletterSubscription} from "./NewsletterSubscription";
 import {ViewCounter} from "./ViewCounter";
 
 export const LandingContent = () => (
   <Container>
-    <h1>Willkommen auf der Webseite</h1>
-    <h2>Diese Seite befindet sich noch im Aufbau</h2>
+    <Grid container>
+      <Grid item xs={12} lg={6}>
+        <h1>Willkommen auf der Webseite</h1>
+        <h2>Diese Seite befindet sich noch im Aufbau</h2>
 
-    <NewsletterSubscription/>
+        <NewsletterSubscription/>
+      </Grid>
+
+      <Grid item xs={12} lg={6} style={{marginTop: 25, textAlign: 'center'}}>
+        <img src="/qr-code.png" style={{maxHeight: 300}} alt="qr-code"/>
+      </Grid>
+
+      <Grid item xs={12}>
+      </Grid>
+    </Grid>
 
     <div style={{position: 'absolute', bottom: 75, right: 25}}>
       <ViewCounter/>
